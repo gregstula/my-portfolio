02eae8d0f7f5a8b98776acb927b841f7485876a1
Greg Stula <gregstula@me.com>
2025-05-26 19:57:49 -0500

    Update css for project cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <div className="flex flex-col min-h-screen w-full bg-gray-100">
       <Header />
-      <h2 className="text-2xl font-bold text-black max-w-4xl mx-auto grid">Projects</h2>
-      <main className="flex-grow p-6 max-w-4xl mx-auto grid gap-20">
+      <main className="flex-grow p-6 max-w-4xl mx-auto space-y-4">
+        <h2 className="text-2xl font-bold mb-4">Projects</h2>
           {projects.map((p, i) => (
           <ProjectCard key={i} project={p} />
         ))}
